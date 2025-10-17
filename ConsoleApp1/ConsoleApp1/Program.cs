2a670d583e08e28de61ac79176b9828fc1d10b4d
iatu <iatu@inbox.ru>
2025-10-17 09:03:01 +0400

    комит 2

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello111, World!");
+Console.WriteLine("Hello1111, World!");
