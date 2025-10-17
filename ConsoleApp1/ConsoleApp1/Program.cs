1cf3d71b7c9062b050366f8d49ada9aed9e71ef4
iatu <iatu@inbox.ru>
2025-10-17 09:03:45 +0400

    комит  3

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -2,3 +2,6 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello111, World!");
 Console.WriteLine("Hello1111, World!");
+
+Console.WriteLine("Hello2222, World!"); 
+Console.WriteLine("Hello55555, World!");
