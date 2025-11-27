e6f713b6be61f33df2d527fb3c0820a300222287
iatu <iatu@inbox.ru>
2025-11-27 20:06:18 +0400

    new2

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -3,6 +3,8 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello111, World!");
 Console.WriteLine("Hello1111, World!");
 
+string str = Console.ReadLine();
+Console.WriteLine(str);
 Console.WriteLine("Hello2222, World!"); 
 Console.WriteLine("Hello55555, World!");
 //варвлоарв
